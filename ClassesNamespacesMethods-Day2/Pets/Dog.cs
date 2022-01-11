0ad39533531aed6b33357550503dcc588f383a1c
DerekMalone <laxmalone@gmail.com>
2022-01-11 15:23:16 -0600

    Add project files.

---
+++ ClassesNamespacesMethods-Day2/Pets/Dog.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesNamespacesMethods_Day2.Pets
+{
+    internal class Dog // internal is similar to private but it limits to only this project seeing it. 
+        // writing in public will make it public.
+    {
+        // properties - TitleCase
+        // auto property - has a plain getter/setter
+        // class is a custom type thus "Dog" is now a type.
+        // any custom property or type we add must be defined by us.
+
+        //access modifier -> public and private. Public can be accessed outside of class. Private can be accessed only inside of class
+             // type
+                     // name
+                            // getter & setter 
+        public string FurType { get; set; } // <-- this is a c# property on a class
+        //string FurType { get; set; } // <-- this is inharently private
+
+        public string Name { get; set; } // when defining properties you must use explicit types
+        public double Weight { get; set; }
+        public string Breed { get; set; }
+
+        public DateTime DateOfBirth { get; private set; } // can have a private setter that can only be done from within the class.
+        // calculated property
+        public int Age { 
+            get
+            {
+                return (DateTime.Now - DateOfBirth).Days / 365;
+            }
+        }       
+
+        // methods - TitleCase
+        //access modifier
+             // type
+                     // name
+                            // code
+       public void Bark() // "void" is what we use when a method does not return anything but it does something
+        {
+            Console.WriteLine($"{Name} says \"Woof Woof\"");  // this is doing something but does not return anything.
+        }
+
+        public void SetDateOfBirth(DateTime dob)
+        {
+            DateOfBirth = dob;
+        }
+
+    }
+}
---
+++ ClassesNamespacesMethods-Day2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassesNamespacesMethods_Day2.Pets;
+
+
+// namespaces 
+
+var dog1 = new Dog(); // must import using "using" since it is in a seperate namespace.
+
+dog1.FurType = "short"; // this is the setter to the type
+
+Console.WriteLine(dog1.FurType); //this is a case of getting the type
+
+dog1.Name = "Fido";
+dog1.Breed = "Pitbull";
+dog1.Weight = 74.5;
+//dog1.DateOfBirth = DateTime.Now.AddDays(-1000);
+dog1.SetDateOfBirth(DateTime.Now.AddDays(-1000)); // way of setting private sett
+
+Console.WriteLine($"{dog1.Name} is a {dog1.Breed} with {dog1.FurType} fur and weighs {dog1.Weight} pounds.");
+
+Dog dog2 = new Dog();
+dog2.FurType = "long";
+dog2.Name = "Astro";
+dog2.Breed = "Husky";
+dog2.Weight = 1.2;
+
+Console.WriteLine($"{dog2.Name} is a {dog2.Breed} with {dog2.FurType} fur and weighs {dog2.Weight} pounds.");
+
+dog2.Bark(); // activating the method that was created. It also has access to the properties in the Bark() Method so the name can be printed.
+
+Console.WriteLine($"{dog1.Name} is {dog1.Age} years old.");
